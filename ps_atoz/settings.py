dfcfde5044e3bafab57c52f5dc00980069d60bfb
GB255 <gabriel.amorim255@gmail.com>
2021-04-24 19:26:54 -0300

    revertido mudança no config

--- ps_atoz/settings.py
+++ ps_atoz/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'ps_atoz.spiders'
 #USER_AGENT = 'ps_atoz (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
