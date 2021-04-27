921f9f6cfa5f880acb54c4109b733fb6a19fe8d1
Rafael Bordin <rafael.bordin@live.com>
2021-04-27 11:53:48 -0300

    Feedback

--- ps_atoz/spiders/busca_spider.py
+++ ps_atoz/spiders/busca_spider.py
@@ -7,8 +7,11 @@ class BuscaSpider(scrapy.Spider):
         'https://www.amazon.com.br/gp/movers-and-shakers/grocery',
     ]
 
+    # Não é boa prática criar funções dentro da classe que não tenham relação
+    # com a funcionalidade da classe. Essas duas funções poderiam ficar em um módulo
+    # diferente, ou fora do escopo da classe.
     def convertInt(self, inteiro):
-
+        # Muito bom usar comentarios docstring
         """
             Converte numeros em inteiros, ex.: 1.200 em 1200
         """
@@ -29,6 +32,11 @@ class BuscaSpider(scrapy.Spider):
             inteiro = int(inteiro)
             return inteiro
 
+    # Essa função esta em desacordo com o principio de que uma função deve apenas realizar uma tarefa.
+    # Ela não converte apenas uma string para float, ela também limpa a string de "R$", por exemplo.
+    # Além disso, a lógica usando "split" é complexa de ler e complicada de dar manutenção.
+    # Poderia ter sido usada a função "replace"
+    # Além disso, a lógica de conversão quebra se o preço tiver separador de milhar e.g. R$1.564,12
     def convertFloat(self, f):
         """
             Converte numeros float, retirando a virgula e colocando um ponto
@@ -56,16 +64,27 @@ class BuscaSpider(scrapy.Spider):
     def parse(self, response, **kwargs):
         for zg in response.css('li.zg-item-immersion'):
             min_price = zg.css('span.p13n-sc-price::text').get()
+            
+            # O nome "max_price" para essa variável pode causar confusão, pois elementos com a classe
+            # apontada existem mesmo que nao haja um preço máximo no produto
             max_price = zg.css('a.a-link-normal.a-text-normal').get()
+            
             if max_price is None:
+                #Não é necessário imprimir None, poderia ter sido usado uma negação no condicional
+                # if max_price is not None para executar apenas quando o max price não fosse None
                 print(None)
             else:
+                # Lógica muito complexa para identiicar se existe um preço máximo, o objetivo de usar scrapy é justamente
+                # não ficar dividindo strings a partir de tags HTML. Poderia ter procurado por elementos span com a classe
+                # p13n-sc-price e se houvessem 2, significa que há max_price
                 t = max_price.split('<span')
                 new_price = t[-1].rstrip('</span></span></a>').split('class="p13n-sc-price">')
                 max_price = new_price[-1]
                 if max_price == min_price:
                     max_price = None
 
+            # É compreensível a intenção de condensar a lógica em uma linha, mas isso cria um código mais complexo do que o necessário 
+            # para ser lido por outras pessoas.
             yield {
                 'name': zg.xpath('span/div/span/a/div/text()').get().strip(),
                 'link': zg.css('span.aok-inline-block.zg-item a::attr(href)').get(),
